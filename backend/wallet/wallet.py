63fa3c4e93f5b6e8e66b6334eb3939457d321d42
Kostas Borotinskas <Kostas.borotinskas@gmail.com>
2021-09-08 13:05:20 +0300

    Transactions tests added

--- backend/wallet/wallet.py
+++ backend/wallet/wallet.py
@@ -15,7 +15,7 @@ class Wallet:
     Allow a miner to authorize transactions.
     """
     def __init__(self):
-        self.adress = str(uuid.uuid4())[0:8]
+        self.address = str(uuid.uuid4())[0:8]
         self.balance = STARTING_BALANCE
         self.private_key = ec.generate_private_key(ec.SECP256K1(), default_backend())
         self.public_key = self.private_key.public_key()
