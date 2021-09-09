447934eb943bd52d7e6facb7315a97ca21b033f8
Kostas Borotinskas <Kostas.borotinskas@gmail.com>
2021-09-09 19:03:30 +0300

    Navigation links added.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Switch, Route } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+
 import './index.css';
 import App from './components/App';
+import Blockchain from './components/Blockchain';
+import ConductTransaction from './components/ConductTransaction';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router history={createBrowserHistory()}>
+      <Switch>
+        <Route path='/' exact component={App}/>
+        <Route path='/blockchain' component={Blockchain}/>
+        <Route path='/conduct-transaction' component={ConductTransaction}/>
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
